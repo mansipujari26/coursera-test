a109219b31eb5be2ac4f7e0ba89166e516fc7e80
MansiPujari <54732541+mansipujari26@users.noreply.github.com>
2020-04-28 23:10:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular.module('lunchChecker',[])
+  .controller('lunchCheckerController', num );
+  num.$inject = ['$scope'];
+  $scope.items = '';
+        {
+
+            $scope.message = '';
+         $scope.check = function ()
+         {
+                let arrayOfItems = $scope.items.split(',');
+                let counter = 0;
+
+                arrayOfItems.forEach((item) => {
+                    if (item != false) {
+                        counter++;
+                    }
+                });
+
+                if (counter > 4)
+                {
+                    $scope.message = 'Enjoy!';
+
+                else if(counter >= 4)
+                {
+                    $scope.message = 'Too much!';
+
+
+                }
+                else
+                {
+                    $scope.message = 'Please enter data first';
+                }
+            }
+        }
+
+    }
+
+})();
